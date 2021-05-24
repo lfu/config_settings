5a3cf5c44e78bd1dd0785882614340f642440720
Lucy Fu <lufu@redhat.com>
2021-05-24 13:26:06 -0400

    Update repo-setup.sh
    Personal info update.

--- setup_scripts/repo-setup.sh
+++ setup_scripts/repo-setup.sh
@@ -6,11 +6,11 @@ cp ./ktest-live ~/foreman
 echo "ktest*" >> ~/foreman/.git/info/exclude
 
 cd ~/katello
-git remote add fork git@github.com:johnpmitsch/katello.git
-git remote add origin https://github.com/katello/katello.git
+git remote add origin git@github.com:lfu/katello.git
+git remote add master https://github.com/katello/katello.git
 #git fetch --all
 
 cd ~/foreman
-git remote add fork git@github.com:johnpmitsch/foreman.git
-git remote add origin https://github.com/foreman/foreman.git
+git remote add origin git@github.com:lfu/foreman.git
+git remote add master https://github.com/foreman/foreman.git
 echo "DONE"
